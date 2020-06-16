80ce551afb278e8d31e3db04e4aa2427a80534fb
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-06-16 23:33:47 +0900

    updated

--- chapter1/kujibiki/main.cpp
+++ chapter1/kujibiki/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-const int MAX_N = 50;
+const int MAX_N = 1000;
+
+bool binary_search(int x, int n, int* kk) {
+    int l = 0, r = n * n;
+    while (r - l >= 1) {
+        int i = (l + r) / 2;
+        if (kk[i] == x) return true;
+        else if (kk[i] < x) l = i + 1;
+        else r = i;
+    }
+    return false;
+}
 
 int main() {
     int n, m, k[MAX_N];
@@ -9,15 +21,20 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin >> k[i];
     }
+
+    int kk[MAX_N * MAX_N];
+    for (int c = 0; c < n; ++c) {
+        for (int d = 0; d < n; ++d) {
+            kk[c * n + d] = k[c] + k[d];
+        }
+    }
+
+    sort(kk, kk + n * n);
     for (int a = 0; a < n; ++a) {
-        for (int b = a; b < n; ++b) {
-            for (int c = b; c < n; ++c) {
-                for (int d = c; d < n; ++d) {
-                    if (k[a] + k[b] + k[c] + k[d] == m) {
-                        cout << "Yes" << endl;
-                        return 0;
-                    }
-                }
+        for (int b = 0; b < n; ++b) {
+            if (binary_search(m - k[a] - k[b], n, kk)) {
+                cout << "Yes" << endl;
+                return 0;
             }
         }
     }
