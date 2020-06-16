e1a84d5aca953de8dec01cf4ba57860af886cf8b
Kakanat <phys.keitaro1227@ruri.waseda.jp>
2020-06-16 23:43:43 +0900

    updated

--- chapter1/kujibiki/main.cpp
+++ chapter1/kujibiki/main.cpp
@@ -5,39 +5,39 @@ using namespace std;
 const int MAX_N = 1000;
 
 bool binary_search(int x, int n, int* kk) {
-    int l = 0, r = n * n;
-    while (r - l >= 1) {
-        int i = (l + r) / 2;
-        if (kk[i] == x) return true;
-        else if (kk[i] < x) l = i + 1;
-        else r = i;
-    }
-    return false;
+  int l = 0, r = n * n;
+  while (r - l >= 1) {
+    int i = (l + r) / 2;
+    if (kk[i] == x) return true;
+    else if (kk[i] < x) l = i + 1;
+    else r = i;
+  }
+  return false;
 }
 
 int main() {
-    int n, m, k[MAX_N];
-    cin >> n >> m;
-    for (int i = 0; i < n; ++i) {
-        cin >> k[i];
-    }
+  int n, m, k[MAX_N];
+  cin >> n >> m;
+  for (int i = 0; i < n; ++i) {
+    cin >> k[i];
+  }
 
-    int kk[MAX_N * MAX_N];
-    for (int c = 0; c < n; ++c) {
-        for (int d = 0; d < n; ++d) {
-            kk[c * n + d] = k[c] + k[d];
-        }
+  int kk[MAX_N * MAX_N];
+  for (int c = 0; c < n; ++c) {
+    for (int d = 0; d < n; ++d) {
+      kk[c * n + d] = k[c] + k[d];
     }
+  }
 
-    sort(kk, kk + n * n);
-    for (int a = 0; a < n; ++a) {
-        for (int b = 0; b < n; ++b) {
-            if (binary_search(m - k[a] - k[b], n, kk)) {
-                cout << "Yes" << endl;
-                return 0;
-            }
-        }
+  sort(kk, kk + n * n);
+  for (int a = 0; a < n; ++a) {
+    for (int b = 0; b < n; ++b) {
+      if (binary_search(m - k[a] - k[b], n, kk)) {
+        cout << "Yes" << endl;
+        return 0;
+      }
     }
-    cout << "No" << endl;
-    return 0;
+  }
+  cout << "No" << endl;
+  return 0;
 }
--- chapter1/triangle/main.cpp
+++ chapter1/triangle/main.cpp
@@ -4,24 +4,24 @@ using namespace std;
 const int MAX_N = 100;
 
 int main() {
-    int n, a[MAX_N];
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
+  int n, a[MAX_N];
+  cin >> n;
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+  int ans = 0;
+  for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; ++j) {
+      for (int k = j + 1; k < n; ++k) {
+        int len = a[i] + a[j] + a[k];
+        int ma = max(a[i], max(a[j], a[k]));
+        int rest = len - ma;
+        if (ma < rest) {
+          ans = max(ans, len);
+      }
     }
-    int ans = 0;
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            for (int k = j + 1; k < n; ++k) {
-                int len = a[i] + a[j] + a[k];
-                int ma = max(a[i], max(a[j], a[k]));
-                int rest = len - ma;
-                if (ma < rest) {
-                    ans = max(ans, len);
-                }
-            }
-        }
     }
-    cout << ans << endl;
-    return 0;
+  }
+  cout << ans << endl;
+  return 0;
 }
